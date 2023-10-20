e435208ae074472df963a95dbb1d5d7090d2f214
Zafar ali <zafaralyjamali722@gmail.com>
2023-10-20 10:59:09 +0500

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,51 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header className='p-4 flex justify-between'>
+        <a href="" className="flex items-center gap-1">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8">
+          <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
+        </svg>
+        <span className='font-bold text-xl'>Zafar</span>
+        </a>
+        <div className='flex gap-2 border border-gray-300 rounded-full py-2 p-4 shadow-md shadow-grey-300'>
+            
+            <div className='p-2 font-bold'>Anywhere</div>
+            <div className= "border-l border-gray-300"></div>
+            <div className='p-2 font-bold'>Any week</div>
+            <div className= "border-l border-gray-300"></div>
+            <div className='p-2 font-bold'>Add guests</div>    
+
+            <button className='bg-primary text-white p-3 rounded-full'>
+
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+              <path d="M8.25 10.875a2.625 2.625 0 115.25 0 2.625 2.625 0 01-5.25 0z" />
+              <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.125 4.5a4.125 4.125 0 102.338 7.524l2.007 2.006a.75.75 0 101.06-1.06l-2.006-2.007a4.125 4.125 0 00-3.399-6.463z" clipRule="evenodd" />
+            </svg>
+
+            </button>
+
+            <dev className="flex gap-2 border border-gray-300 rounded-full py-2 p-4 shadow-md shadow-grey-300" >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+            <path fillRule="evenodd" d="M3 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 5.25zm0 4.5A.75.75 0 013.75 9h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 9.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
+            </svg>
+
+            <div className='bg-gray-500 text-white rounded-full border-grey-500 overflow-hidden'>
+
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+            <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clipRule="evenodd" />
+            </svg>
+
+            </div>
+
+            </dev>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default App
