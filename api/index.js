b94c8b6272d29ebd6ababe1b168f2cf4b1efd16d
zafar ali <zafaralyjamali722@gmail.com>
2023-10-30 22:47:44 +0500

    11th/-  Completed the places section only front-end part all the buttons fields

--- api/index.js
+++ api/index.js
@@ -27,8 +27,8 @@ app.use(
 
 mongoose
   .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true, // Add these options
-    useUnifiedTopology: true, // Add these options
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to MongoDB");
