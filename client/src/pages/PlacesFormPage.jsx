377dc17a6004bc2485696b994decb6434a212e40
Zafar Ali <zafaralyjamali722@gmail.com>
2023-11-03 15:11:49 +0500

    code cleaning code refactring code sepration apart from booking section redirection work

---
+++ client/src/pages/PlacesFormPage.jsx
@@ -0,0 +1,138 @@
+import { useState } from "react";
+import Perks from "../Perks";
+import PhotosUploader from "../PhotosUploader";
+import axios from "axios";
+import AccountNav from "../AccountNav";
+import { Navigate } from "react-router-dom";
+
+export default function PlacesFormPage() {
+  const [title, setTitle] = useState("");
+  const [address, setAddress] = useState("");
+  const [addedPhotos, setAddedPhotos] = useState([]);
+
+  const [description, setDescription] = useState("");
+  const [perks, setPerks] = useState([]);
+  const [extraInfo, setExtraInfo] = useState("");
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [maxGuests, setMaxGuests] = useState(1);
+  const [redirect, setRedirect] = useState(false);
+
+  function inputHeader(text) {
+    return <h2 className="text-2xl mt-4">{text}</h2>;
+  }
+  function inputDescription(text) {
+    return <p className="text-gray-500 text-sm"> {text}</p>;
+  }
+
+  function preInput(header, description) {
+    return (
+      <>
+        {inputHeader(header)}
+        {inputDescription(description)}
+      </>
+    );
+  }
+
+  async function addNewPlace(ev) {
+    ev.preventDefault();
+
+    await axios.post("/places", {
+      title,
+      address,
+      addedPhotos,
+      description,
+      perks,
+      extraInfo,
+      checkIn,
+      checkOut,
+      maxGuests,
+    });
+
+    setRedirect(true);
+  }
+
+  if (redirect) {
+    return <Navigate to={"/account/places"} />;
+  }
+
+  return (
+    <div style={{ marginTop: "20px" }}>
+      <AccountNav />
+      <form onSubmit={addNewPlace}>
+        {preInput(
+          "Title",
+          "Title should be short and catchy as in advertisement"
+        )}
+        <input
+          value={title}
+          onChange={(ev) => setTitle(ev.target.value)}
+          type="text"
+          placeholder="Ex: My lovely apartment"
+          style={{ width: "100%" }}
+        />
+        {preInput("Address", "Address of the place")}
+        <input
+          type="text"
+          value={address}
+          onChange={(ev) => setAddress(ev.target.value)}
+          placeholder="Address"
+        />
+        {preInput("Photos", "High-quality/The more the better/Detailed")}
+        <PhotosUploader addedPhotos={addedPhotos} onChange={setAddedPhotos} />
+
+        {preInput("Description", "About the place")}
+        <textarea
+          value={description}
+          onChange={(ev) => setDescription(ev.target.value)}
+        />
+        {preInput("Perks", "Select all the perks of your place")}
+        <div className="grid mt-2 gap-2 grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
+          <Perks selected={perks} onChange={setPerks} />
+        </div>
+        {preInput("Extra info", "Rules/Terms and conditions")}
+        <textarea
+          value={extraInfo}
+          onChange={(ev) => setExtraInfo(ev.target.value)}
+        />
+
+        {preInput(
+          "Check in & out time",
+          "When formulating our reservation policies, it is essential to establish specific check-in and check-out times, with due consideration for an allotted timeframe dedicated to room cleaning between successive guests"
+        )}
+
+        <div className="grid sm:grid-cols-3 gap-2">
+          <div>
+            <h3 className="mt-2 -mb-1">Check in time</h3>
+            <input
+              type="text"
+              value={checkIn}
+              onChange={(ev) => setCheckIn(ev.target.value)}
+              placeholder="10:30 AM"
+            />
+          </div>
+          <div>
+            <h3 className="mt-2 -mb-1">Check out time</h3>
+            <input
+              type="text"
+              value={checkOut}
+              onChange={(ev) => setCheckOut(ev.target.value)}
+              placeholder="12:30 PM"
+            />
+          </div>
+          <div>
+            <h3 className="mt-2 -mb-1">Max guests</h3>
+            <input
+              type="number"
+              value={maxGuests}
+              onChange={(ev) => setMaxGuests(ev.target.value)}
+            />
+          </div>
+        </div>
+        <div>
+          <button className="primary my-4"> Submit </button>
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ client/src/pages/ProfilePage.jsx
@@ -0,0 +1,49 @@
+import { useContext } from "react";
+import { UserContext } from "../UserContext"; // Adjust the import path as needed
+import { Navigate, useParams } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import PlacesPage from "./PlacesPage";
+import AccountNav from "../AccountNav";
+export default function ProfilePage() {
+  const [redirect, setRedirect] = useState(null);
+  const { ready, user, setUser } = useContext(UserContext);
+  let { subpage } = useParams();
+
+  if (subpage === undefined) {
+    subpage = "profile";
+  }
+
+  async function logout() {
+    await axios.post("/logout");
+    setRedirect("/");
+    setUser(null);
+  }
+
+  if (!ready) {
+    return "Loading....";
+  }
+
+  if (ready && !user && !redirect) {
+    return <Navigate to={"/login"} />;
+  }
+
+  if (redirect) {
+    return <Navigate to={redirect} />;
+  }
+  return (
+    <div>
+      <AccountNav />
+      {subpage === "profile" && (
+        <div className="text-center max-w-lg mx-auto">
+          logged in as {user.first} {user.last} ({user.email})
+          <button onClick={logout} className="primary max-w-sm mt-2">
+            {" "}
+            Logout{" "}
+          </button>
+        </div>
+      )}
+      {subpage === "places" && <PlacesPage />}
+    </div>
+  );
+}
