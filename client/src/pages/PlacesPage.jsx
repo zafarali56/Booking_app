f922ccf26bd77542957e56015e6b17f70bb0919a
Zafar Ali <zafaralyjamali722@gmail.com>
2023-11-04 15:14:59 +0500

    Fixed* the bug of link picture not showing up Everything works

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AccountNav from "../AccountNav";
 import axios from "axios";
-import { useState } from "react";
+
 export default function PlacesPage() {
   const [places, setPlaces] = useState([]);
+
   useEffect(() => {
     axios.get("/places").then(({ data }) => {
       setPlaces(data);
     });
   }, []);
+
   return (
     <div>
       <AccountNav />
@@ -36,30 +38,52 @@ export default function PlacesPage() {
           Add New Places
         </Link>
       </div>
+
       <div className="mt-4">
         {places.length > 0 &&
-          places.map((place, index) => (
-            <Link
-              to={"/account/places/" + place._id}
-              className="flex cursor-pointer gap-4 bg-gray-100 rounded-xl p-5"
-              key={index}
-            >
-              <div className="flex w-32 h-32 bg-gray-400 grow shrink-0 rounded-xl">
-                {place.photos.length > 0 && (
-                  <img
-                    className="object-cover rounded-2xl"
-                    src={"http://localhost:4000/uploads/" + place.photos[0]}
-                    alt=""
-                  />
-                )}
-              </div>
+          places.map((place, index) => {
+            // Extract the image address and file name
+            const imageAddress = place.photos[0];
+            console.log("Image Address:", imageAddress);
+            let fileName;
 
-              <div className="grow-0 shrink">
-                <h2 className="text-xl ">{place.title}</h2>
-                <p className="text-sm mt-2">{place.description}</p>
+            // Check if the image address contains a specific part
+            if (
+              imageAddress.includes(
+                "/home/zafar/Documents/CODE/MERN_BookingApp/api/uploads"
+              )
+            ) {
+              fileName = imageAddress.split("/").pop();
+            } else {
+              fileName = imageAddress;
+            }
+
+            console.log("File Name:", fileName);
+
+            return (
+              <div key={index}>
+                <Link
+                  to={"/account/places/" + place._id}
+                  className="flex cursor-pointer gap-4 bg-gray-100 rounded-xl p-5"
+                >
+                  <div className="flex w-32 h-32 bg-gray-400 grow shrink-0 rounded-xl">
+                    {place.photos.length > 0 && (
+                      <img
+                        className="object-cover rounded-2xl"
+                        src={"http://localhost:4000/uploads/" + fileName}
+                        alt=""
+                      />
+                    )}
+                  </div>
+
+                  <div className="grow-0 shrink">
+                    <h2 className="text-xl ">{place.title}</h2>
+                    <p className="text-sm mt-2">{place.description}</p>
+                  </div>
+                </Link>
               </div>
-            </Link>
-          ))}
+            );
+          })}
       </div>
     </div>
   );
