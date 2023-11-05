5292c605935a56a0c5f5a5809246dd511d8ca74f
Zafar Ali <zafaralyjamali722@gmail.com>
2023-11-05 19:52:02 +0500

    Added Photos in form can be deleted, and cover photo can be replaced by clicking on the star button

--- client/src/PhotosUploader.jsx
+++ client/src/PhotosUploader.jsx
@@ -57,13 +57,84 @@ export default function PhotosUploader({ addedPhotos, onChange }) {
             // Split the link by '/' and get the last part (the filename)
             const filename = link.split("/").pop();
 
+            function removePhoto(ev, filename) {
+              ev.preventDefault();
+              onChange([...addedPhotos.filter((photo) => photo !== filename)]);
+            }
+
+            function selectAsMainPhoto(ev, filename) {
+              ev.preventDefault();
+
+              onChange([
+                filename,
+                ...addedPhotos.filter((photo) => photo !== filename),
+              ]);
+            }
+
             return (
-              <div className="h-32 flex" key={link}>
+              <div className="h-32 flex relative" key={link}>
                 <img
                   className="rounded-2xl w-full object-cover position-center"
                   src={"http://localhost:4000/uploads/" + filename}
                   alt="description"
                 />
+                <button
+                  onClick={(ev) => removePhoto(ev, link)}
+                  className="absolute bottom-2 right-2 cursor-pointer text-red-600 bg-inherit"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path d="M2 3a1 1 0 00-1 1v1a1 1 0 001 1h16a1 1 0 001-1V4a1 1 0 00-1-1H2z" />
+                    <path
+                      fillRule="evenodd"
+                      d="M2 7.5h16l-.811 7.71a2 2 0 01-1.99 1.79H4.802a2 2 0 01-1.99-1.79L2 7.5zm5.22 1.72a.75.75 0 011.06 0L10 10.94l1.72-1.72a.75.75 0 111.06 1.06L11.06 12l1.72 1.72a.75.75 0 11-1.06 1.06L10 13.06l-1.72 1.72a.75.75 0 01-1.06-1.06L8.94 12l-1.72-1.72a.75.75 0 010-1.06z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+                <button
+                  onClick={(ev) => selectAsMainPhoto(ev, link)}
+                  className="absolute bottom-2 left-2 cursor-pointer text-white bg-inherit"
+                >
+                  {link === addedPhotos[0] && (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  )}
+                  {link !== addedPhotos[0] && (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                      />
+                    </svg>
+                  )}
+
+                  <span className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 pointer-events-none group-hover:opacity-100">
+                    Add to cover
+                  </span>
+                </button>
               </div>
             );
           })}
