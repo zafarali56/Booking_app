815dfc586251a74b9d8028803eee8386dc9d7e5c
Zafar Ali <zafaralyjamali722@gmail.com>
2023-11-10 01:52:32 +0500

    tweaks

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -28,8 +28,8 @@ export default function Header() {
         <span className="font-bold text-xl">Table Trove</span>
       </Link>
 
-      <div className="flex gap-2 border border-gray-300 rounded-full py-2 p-4 shadow-md items-center shadow-grey-300 ">
-        <div className="flex border border-gray-400 rounded-full shrink-0 ">
+      <div className="flex gap-1 border border-gray-300 rounded-full py-2 p-3 shadow-md items-center shadow-grey-300 ">
+        <div className="flex border border-gray-300 rounded-full ">
           <div className="p-2 font-bold">Anywhere</div>
           <div className="border-l border-gray-400 rounded-full"></div>
           <div className="p-2 font-bold">Any week</div>
@@ -51,45 +51,46 @@ export default function Header() {
               />
             </svg>
           </button>
-        </div>
-        <Link
-          to={user ? "/account" : "/login"}
-          className="flex gap-2 border border-gray-300 rounded-full py-2 p-4 shadow-md items-center shadow-grey-300"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6 custom-hidden"
-          >
-            <path
-              fillRule="evenodd"
-              d="M3 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 5.25zm0 4.5A.75.75 0 013.75 9h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 9.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-              clipRule="evenodd"
-            />
-          </svg>
 
-          <div className="bg-gray-500 text-white rounded-full border-grey-500 flex items-center">
+          <Link
+            to={user ? "/account" : "/login"}
+            className="flex gap-2 border border-gray-300 rounded-full py-2 p-4 shadow-md items-center shadow-grey-300"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
-              className="w-6 h-6"
+              className="w-6 h-6 custom-hidden"
             >
               <path
                 fillRule="evenodd"
-                d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                d="M3 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 5.25zm0 4.5A.75.75 0 013.75 9h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 9.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
                 clipRule="evenodd"
               />
             </svg>
-          </div>
 
-          {!!user && (
-            <div className="custom-hidden">
-              {user.first} {user.last}
+            <div className="bg-gray-500 text-white rounded-full border-grey-500 flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
             </div>
-          )}
-        </Link>
+
+            {!!user && (
+              <div className="custom-hidden">
+                {user.first} {user.last}
+              </div>
+            )}
+          </Link>
+        </div>
       </div>
     </header>
   );
