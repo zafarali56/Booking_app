dd37e7f5287a68e38059622910e5dc3e081931ff
Zafar Ali <zafaralyjamali722@gmail.com>
2023-11-11 10:11:10 +0500

    padding issue fixed

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -45,7 +45,7 @@ export default function Header() {
               />
             </svg>
 
-            <div className="p-2 font-semibold custom-hidden_2">Anywhere</div>
+            <div className="px-2 font-semibold custom-hidden_2">Anywhere</div>
           </div>
 
           <div className="flex items-center border border-gray-500 rounded-full px-2 ">
@@ -64,7 +64,7 @@ export default function Header() {
               />
             </svg>
 
-            <div className="p-2 font-semibold custom-hidden_2">Any week</div>
+            <div className="px-2 font-semibold custom-hidden_2">Any week</div>
           </div>
 
           <div className="flex items-center border border-gray-500 rounded-full px-2">
@@ -88,18 +88,19 @@ export default function Header() {
             <div className="p-2 font-semibold custom-hidden_2">Add guests</div>
           </div>
 
-          <button className="bg-primary text-white p-4 rounded-full">
+          <button className="bg-primary text-white px-3 rounded-full">
             <svg
               xmlns="http://www.w3.org/2000/svg"
+              fill="none"
               viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-4 h-4"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
             >
-              <path d="M8.25 10.875a2.625 2.625 0 115.25 0 2.625 2.625 0 01-5.25 0z" />
               <path
-                fillRule="evenodd"
-                d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.125 4.5a4.125 4.125 0 102.338 7.524l2.007 2.006a.75.75 0 101.06-1.06l-2.006-2.007a4.125 4.125 0 00-3.399-6.463z"
-                clipRule="evenodd"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
               />
             </svg>
           </button>
